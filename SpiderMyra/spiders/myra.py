c89da935427e41525e61003c33d242eeed2299fd
Ray Mac <supremonett@gmail.comm>
2021-07-04 20:35:01 -0300

    coletando quotes

--- SpiderMyra/spiders/myra.py
+++ SpiderMyra/spiders/myra.py
@@ -14,20 +14,25 @@ class MyraSpider(scrapy.Spider):
         cont = 0
         for url in urls:
             if(cont == 0):
+                print('------------ Logando no Site --------------')
                 yield scrapy.Request(url=urls[cont], callback=self.parse)
             else:
                 yield scrapy.Request(url=urls[cont], callback=self.coletarDados)
             cont += 1
 
     def parse(self, response):
-        log = '--------------- LOGADO ------------------'
         token = response.xpath('//div[@class="row header-box"]/a/@href').get()
         return FormRequest.from_response(response, formdata={
             'csrf_token': token,
             'username': 'supremonett@gmail.com',
             'password': '1234',
-        }, callback=print(log))
+        })
 
 
     def coletarDados(self, response):
-        print('dados')
+        print('---------- COLETANDO DADOS -------------')
+        for i in response.xpath('//div[@class="quote"]'):
+            texto = i.xpath('.//span[@class="text"]/text()').get(),
+            autor = i.xpath('.//small[@class="author"]/text()').get(),
+            tags = i.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall()
+            print(tags)
